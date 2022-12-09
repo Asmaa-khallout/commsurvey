58313db22a0ac2e65941a1d04723ad319f739679
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-12-09 15:14:26 +0100

    Add modal confirmation

--- survey_sign/models/survey_user_input_inherit.py
+++ survey_sign/models/survey_user_input_inherit.py
@@ -8,7 +8,7 @@ class SurveySurveyInherit(models.Model):
     signature = fields.Image('Signature', help='Signature received through the portal.', copy=False, attachment=True, max_width=1024, max_height=1024)
     signed_by = fields.Char('Signed By', help='Name of the person that signed the SO.', copy=False)
     signed_on = fields.Datetime('Signed On', help='Date of the signature.', copy=False)
-    state_signature = fields.Selection([('progress','En cours')
+    state_signature = fields.Selection([('progress','En cours'),
         ('signe', 'Signé'),
         ('not_signe', 'Non Signé')], string='Statut de signature', default='progress', compute="_get_statut",
         store=True)
